26a0e8fc8f571a78921b991c992793cdc323f495
RomainTranchant <romaintranchant@me.com>
2024-10-17 08:21:40 -0500

    Update Problem 4 String Reversal.py

--- Problem 4 String Reversal.py
+++ Problem 4 String Reversal.py
@@ -7,7 +7,24 @@
 # Write a Python function called reverse_string that takes a string as input and returns the string
 # reversed. For example, if the input is "hello", the function should return "olleh".
 
+# Create a reverse_string function using the_string as a parameter
+def reverse_string(the_string):
+
+# Print the original string
+    print(f"The original string is: {the_string}")
+  
+# Create an empty string to get the reversed string 
+    reversed_string = ""
+  
+# Use a for loop to go through the original string and slice to reverse the original string
+    for char in the_string[::-1]:
+
+# Append each character to the reversed_string and print the reversed string
+        reversed_string += char
+    print(f"The reversed string is: {reversed_string}")
+
+# Define the original string
 the_string = "Hello"
-print(f"The original string is: {the_string}")
-reversed_string = the_string[::-1]
-print(f"The reversed string is: {reversed_string}")
+
+# Call the function using the string as a parameter
+reverse_string(the_string)
